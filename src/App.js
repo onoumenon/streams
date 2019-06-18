aed8f61e6f220a9220fccaaeb23acbfc82452e5e
onoumenon <tanhuitian@gmail.com>
2019-06-18 23:29:55 +0800

    Create streams action creators and reducers, create browser history

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./components/streams/StreamCreate";
 import StreamDelete from "./components/streams/StreamDelete";
 import StreamEdit from "./components/streams/StreamEdit";
 import StreamList from "./components/streams/StreamList";
 import StreamShow from "./components/streams/StreamShow";
 import Header from "./components/Header";
+import history from "./history";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Router history={history}>
         <>
           <Header />
           <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/edit/:id" component={StreamEdit} />
           <Route path="/streams/delete" component={StreamDelete} />
           <Route path="/streams/show" component={StreamShow} />
           <Route path="/" exact component={StreamList} />
         </>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
